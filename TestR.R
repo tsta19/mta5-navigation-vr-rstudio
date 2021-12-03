190eb0536f0c0cae947465e77e5c5d61fe07f7d0
reptinias <32259405+reptinias@users.noreply.github.com>
2021-12-03 09:36:37 +0100

    Update af navne og r-filen
    ^^^^

--- TestR.R
+++ TestR.R
@@ -2,7 +2,7 @@ library(rio)
 library(utils)
 
 #Loader en csv fil
-rio_csv = import("C:/Users/kspar/OneDrive/Dokumenter/GitHub/mta5-navigation-vr-rstudio/DataLogging/HopefullyLast3.csv")
+rio_csv = import("C:/Users/kspar/OneDrive/Dokumenter/GitHub/mta5-navigation-vr-rstudio/test2.csv")
 head(rio_csv)
 View(rio_csv)
 
@@ -11,3 +11,8 @@ plot(rio_csv)
 
 str(rio_csv)
 summary(rio_csv)
+
+#Ændre data i dataframe
+rio_csv$TestID = 1
+
+write.csv(rio_csv,"C:/Users/kspar/OneDrive/Dokumenter/GitHub/mta5-navigation-vr-rstudio/test2Updata.csv", row.names = FALSE)
