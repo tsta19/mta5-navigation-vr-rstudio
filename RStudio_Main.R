4f1accca05aea9735bfd1249aee8906766907575
poul <ktromb19@student.aau.dk>
2021-12-08 14:32:16 +0100

    Merge script xD

--- RStudio_Main.R
+++ RStudio_Main.R
@@ -3,13 +3,23 @@ library(utils)
 
 install.packages("dplyr")
 
-rio_csv = import("C:/Users/thoma/Desktop/Github Repositories/mta5-navigation-vr-rstudio/Testparticipents/TP2.csv")
+rio_csv = import("/Users/tromborg/Desktop/Testparticipents - Kopi")
 head(rio_csv)
 View(rio_csv)
+
+#Script til at merge csv
+library(data.table)
+setwd("/Users/tromborg/Desktop/Mergemap")
+files <- list.files(pattern = ".csv")
+temp <- lapply(files, fread, sep=";")
+data <- rbindlist( temp )
+write.csv(data, file="mergecsv.csv", row.names = FALSE)
+
 
 #Laver plots i par af alt dataen i dataframet
 plot(rio_csv)
 
 str(rio_csv)
 summary(rio_csv)
+
 
